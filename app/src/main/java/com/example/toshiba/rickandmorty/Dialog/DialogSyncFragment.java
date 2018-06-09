c21174ecfec3bd2ccd40b0a1d1d2f2968c1d4d82
Omar T.Domínguez <otorresd1gmail.com>
2018-06-08 18:53:31 -0700

    Se mejoro visualmente el dialogo de sincronizacion y se agrego un menu
    Queda por realizar:
    
    2- Mostrar el resto de la informacion basica: Episodes
    6- implementar las acciones del menu:
        6.1- Un filtro
        6.2- Un eliminar base de datos
        6.3- sincronizar
    7- Mejorar el codigo

---
+++ app/src/main/java/com/example/toshiba/rickandmorty/Dialog/DialogSyncFragment.java
@@ -0,0 +1,58 @@
+package com.example.toshiba.rickandmorty.Dialog;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.NumberPicker;
+
+import com.example.toshiba.rickandmorty.CallBack.RickAndMortyCallBack;
+import com.example.toshiba.rickandmorty.Database.Controller;
+import com.example.toshiba.rickandmorty.R;
+
+public class DialogSyncFragment extends DialogFragment{
+    private NumberPicker numberPicker;
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity(), R.style.MyDialog);
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.fragment_dialog_sync, null);
+        numberPicker = (NumberPicker)view.findViewById(R.id.numberPicker2);
+        numberPicker.setMaxValue(25);
+        numberPicker.setMinValue(1);
+        builder.setView(view)
+                .setPositiveButton(R.string.accept_button, new DialogInterface.OnClickListener(){
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        download(numberPicker.getValue());
+                        DialogSyncFragment.this.getDialog().cancel();
+                    }
+                })
+                .setNegativeButton(R.string.cancel_button, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                DialogSyncFragment.this.getDialog().cancel();
+            }
+        }).setTitle("Sync pages").setIcon(R.mipmap.ic_circle_rick_and_morty);
+
+        return builder.create();
+    }
+
+    public void download(int page){
+        Controller controller = new Controller(getActivity());
+        RickAndMortyCallBack postsCallBack = new RickAndMortyCallBack(getActivity(), controller);
+        for(int i = 1; i <= page; i++){
+            postsCallBack.start(i);
+        }
+    }
+}
--- app/src/main/java/com/example/toshiba/rickandmorty/MainActivity.java
+++ app/src/main/java/com/example/toshiba/rickandmorty/MainActivity.java
@@ -5,9 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import com.example.toshiba.rickandmorty.Adapter.MainRecyclerAdapter;
-import com.example.toshiba.rickandmorty.CallBack.RickAndMortyCallBack;
 import com.example.toshiba.rickandmorty.Database.Character;
 import com.example.toshiba.rickandmorty.Database.Controller;
 import com.example.toshiba.rickandmorty.Dialog.DialogSyncFragment;
@@ -23,6 +25,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        myToolbar.setSubtitleTextAppearance(this, R.style.AppTheme_ActionBar);
+        setSupportActionBar(myToolbar);
 
         FragmentManager fragmentManager = getSupportFragmentManager();
         DialogSyncFragment dialogSyncFragment = new DialogSyncFragment();
@@ -42,5 +48,14 @@ public class MainActivity extends AppCompatActivity {
         mAdapter = new MainRecyclerAdapter(list);
         mRecyclerView.setAdapter(mAdapter);
 
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
 }
